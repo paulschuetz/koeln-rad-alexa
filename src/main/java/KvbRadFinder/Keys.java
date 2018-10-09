b013bdefa7e3611c9ed12091c50c10d139c49507
Paul <paulschuetz.mail@gmail.com>
2018-10-09 16:40:15 +0200

    ignore keys

--- src/main/java/KvbRadFinder/Keys.java
+++
@@ -1,6 +0,0 @@
-package KvbRadFinder;
-
-public class Keys {
-    public static final String MAPQUEST_API_KEY = "YOUR-MAP-QUEST-API-KEY-GOES-HERE";
-    public static final String GOOGLE_MAPS_STATIC_API_KEY = "YOUR-GOOGLE-STATIC-MAPS-KEY-GOES-HERE";
-}
